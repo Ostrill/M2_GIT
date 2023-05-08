d2789cbcce2e6ad4a70a0941c835ddfd7d666d5b
Vladimir <vovcrane@mai.ru>
2023-05-08 21:04:25 +0300

    Add function for extracting part of speech

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from pymystem3 import Mystem
+import re
+
+
+def extract_and_check_part(gr):
+    required_parts = {'A', 'ADV', 'V'}
+    part_of_speech = re.split(r'\W+', gr)[0]
+    if part_of_speech in required_parts:
+        return part_of_speech
+    else:
+        return None
+
 
 with open('text.txt', 'r') as file:
     text = file.read()
