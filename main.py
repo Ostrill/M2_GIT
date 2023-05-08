ccb8a30ba054753ef27ea7a973596eb83efeff81
Vladimir <vovcrane@mai.ru>
2023-05-08 20:51:42 +0300

    Print part of speech for each word

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ from pymystem3 import Mystem
 with open('text.txt', 'r') as file:
     text = file.read()
 
-print(text)
+ms = Mystem()
+analysis = ms.analyze(text)
+
+for token in analysis:
+    word = token.get('analysis')
+    if not word:
+        continue
+    for data in word:
+        if isinstance(data, dict):
+            gr = data.get('gr')
+            if gr:
+                print(gr)
+                break
